60ae61fbc2d99b1fb8d85245f2084b88980771c5
Matt Kirwan <mail@mattkirwan.com>
2014-11-07 11:03:16 +0000

    Watcher now working and notifying output

--- asset-builder.go
+++ asset-builder.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-    "fmt"
     "gopkg.in/fsnotify.v1"
+    "log"
 )
 
-func main() {
-    
-    watcher, err = fsnotify.NewWatcher()
+var watcher, err = fsnotify.NewWatcher()
+
+func main() {    
 
     if err != nil {
         log.Fatal(err)
     }
 
+    defer watcher.Close()
 
+    done := make(chan bool)
+
+    go func() {
+        for {
+            select {
+            case event := <-watcher.Events:
+                log.Println("Event:", event)
+                if event.Op&fsnotify.Write == fsnotify.Write {
+                    log.Println("Modified file:", event.Name)
+                }
+            case err := <-watcher.Errors:
+                log.Println("Error:", err)
+            }
+        }
+    }()
+
+    err = watcher.Add("/home/mattkirwan/")
+    
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    <-done
 }
