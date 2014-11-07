6f4554aa3d3556b76063c4765e22a73043feebf5
Matt Kirwan <mail@mattkirwan.com>
2014-11-07 14:48:17 +0000

    Started adding some heavy comments to digest it

--- asset-builder.go
+++ asset-builder.go
@@ -19,14 +19,35 @@ func main() {
 
     go func() {
         for {
+
+            // The 'select' statement rather beautifully allows us to wait on multiple go routines
+            // it blocks until a case is matched and then runs that routine.
             select {
-            case event := <-watcher.Events:
-                log.Println("Event:", event)
-                if event.Op&fsnotify.Write == fsnotify.Write {
-                    log.Println("Modified file:", event.Name)
-                }
-            case err := <-watcher.Errors:
-                log.Println("Error:", err)
+
+                // var watcher is a struct type with two fields; Events & Errors
+                // Both Events & Errors are channel types meaning we can receive values
+                // from the channel and store in our program.
+
+                // An event was detected by the blocking select, so let's grab that communication value from Events
+                // In this case watcher.Events receives an 'Event' which is a struct type representing a single file
+                // system notification. It contains two fields; Name & Op
+                case event := <-watcher.Events:
+
+                    // Name is a string with a relative path to the file or directory that triggered the event and Op is the
+                    // particular operation that triggered the Event.
+                    // We can access the String() method (which implements the Stringer interface) because Println checks for
+                    // any methods on the struct that implement it and therefore return a nicely formatted event string by
+                    // just using the actual struct.
+                    log.Println("Event Triggered: ", event)
+
+                    // 
+                    if event.Op&fsnotify.Write == fsnotify.Write {
+                        log.Println("File Modified:", event.Name)
+                    }
+
+                // An error event was detected by the blocking select.    
+                case err := <-watcher.Errors:
+                    log.Println("Error:", err)
             }
         }
     }()
